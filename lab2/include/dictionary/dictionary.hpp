78ec43fb1129c2bb701390bb685e74d63b9603cf
litc0x3B <ur.waifu.is.explosive.loli@gmail.com>
2023-01-19 23:31:34 +0300

    changed shared_ptr to raw pointers in Dictionary

--- lab2/include/dictionary/dictionary.hpp
+++ lab2/include/dictionary/dictionary.hpp
@@ -14,22 +14,21 @@ template <class TKey, class TValue>
 class Dictionary : public IDictionary<TKey, TValue>
 {
  private:
-  using DictTree = BinTree<std::shared_ptr<BinTreeDictEntry<TKey>>>;
+  using DictTree = BinTree<BinTreeDictEntry<TKey> *>;
   using EntryBase = BinTreeDictEntry<TKey>;
   using Entry = BinTreeDictEntryKeyAndValue<TKey, TValue>;
   using EntryKey = BinTreeDictEntryOnlyKey<TKey>;
 
   Dictionary *_CopyImpl() const override { return new Dictionary(*this); };
+
   const CompKeysFunc<TKey> cmp;
   std::shared_ptr<DictTree> binTree;
 
  public:
-  Dictionary(const CompKeysFunc<TKey> &cmp) : cmp(cmp)
+  explicit Dictionary(const CompKeysFunc<TKey> &cmp) : cmp(cmp)
   {
-    binTree = std::make_shared<DictTree>(
-        [cmp](const std::shared_ptr<EntryBase> &entry1,
-              const std::shared_ptr<EntryBase> &entry2)
-        { return cmp(entry1->GetKey(), entry2->GetKey()); });
+    binTree = std::make_shared<DictTree>([cmp](const EntryBase *entry1, const EntryBase *entry2)
+                                         { return cmp(entry1->GetKey(), entry2->GetKey()); });
   }
 
   Dictionary(const CompKeysFunc<TKey> &cmp,
@@ -44,61 +43,74 @@ class Dictionary : public IDictionary<TKey, TValue>
 
   Dictionary(const Dictionary &dict) : Dictionary(dict.GetComparerFunc())
   {
-    this->binTree = dict.binTree->Copy();
+    dict.ForEach([this](const TKey &key, const TValue &value) { this->Add(key, value); });
   }
 
-  std::shared_ptr<Dictionary> Copy() const
+  ~Dictionary()
   {
-    return std::shared_ptr<Dictionary>(_CopyImpl());
+    binTree->ForEach([](const EntryBase *entry) { delete entry; });
   }
 
+  Dictionary &operator=(const Dictionary &dict) = delete;
+
+  std::shared_ptr<Dictionary> Copy() const { return std::shared_ptr<Dictionary>(_CopyImpl()); }
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnreachableCode"
   Nullable<TValue> Get(TKey key) const override
   {
-    auto entry = std::make_shared<EntryKey>(key);
-    std::shared_ptr<EntryBase> found;
+    auto *entry = new EntryKey(key);
+    EntryBase *found = nullptr;
     binTree->Search(entry, found);
+    delete entry;
+
     if (found == nullptr)
     {
       return Nullable<TValue>::Null;
     }
-    return std::dynamic_pointer_cast<Entry>(found)->GetValue();
+
+    TValue retValue = dynamic_cast<Entry *>(found)->GetValue();
+    return retValue;
   }
+#pragma clang diagnostic pop
 
   void ForEach(const std::function<void(const TValue &)> &func) const override
   {
     binTree->ForEach(
-        [func](const std::shared_ptr<EntryBase> &entryBase)
+        [func](const EntryBase *entryBase)
         {
-          auto entry = std::dynamic_pointer_cast<Entry>(entryBase);
+          auto *entry = dynamic_cast<const Entry *>(entryBase);
           func(entry->GetValue());
         });
   }
 
-  void ForEach(
-      const std::function<void(const TKey &key, const TValue &value)> &func) const
+  void ForEach(const std::function<void(const TKey &key, const TValue &value)> &func) const
   {
     binTree->ForEach(
-        [func](const std::shared_ptr<EntryBase> &entryBase)
+        [func](const EntryBase *entryBase)
         {
-          auto entry = std::dynamic_pointer_cast<Entry>(entryBase);
+          auto *entry = dynamic_cast<const Entry *>(entryBase);
           func(entry->GetKey(), entry->GetValue());
         });
   }
 
   void Add(const TKey &key, const TValue &value)
   {
-    auto entry = std::make_shared<Entry>(key, value);
+    auto *entry = new Entry(key, value);
     binTree->Add(entry);
   }
 
   Nullable<TValue> Remove(TKey key) override
   {
-    auto entry = std::make_shared<EntryKey>(key);
-    auto removedEntry = binTree->Remove(entry);
+    auto *entry = new EntryKey(key);
+    Nullable<EntryBase *> removedEntry = binTree->Remove(entry);
+    delete entry;
+
     if (!removedEntry.IsNull())
     {
-      return std::dynamic_pointer_cast<Entry>(removedEntry.GetValue())
-          ->GetValue();
+      TValue foundValue = dynamic_cast<Entry *>(removedEntry.GetValue())->GetValue();
+      delete removedEntry.GetValue();
+      return foundValue;
     }
 
     else
